c342c637337c7afadb2f61ae3b7532e479ddebf9
Oat Milk <exasperatedoatmilk@gmail.com>
2021-06-17 16:46:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+import time
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+  print("I Am Online")
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Ok Leave Now, Thanks"))
+  await client.get_channel(855175532140167188).send("Hello! I has becomes onlines")
+
+
+@client.event
+
+async def on_message(message):
+  await client.get_channel(855175532140167188).send(message.content)
+  
+
+
+client.run(os.getenv('TOKEN'))
